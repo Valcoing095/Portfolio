f6d8231c8aed015b43f33fffee354c22d9d9f40c
Yeison <yvelasco321@gmail.com>
2025-01-02 17:48:53 -0500

    configuración portafolio en laravel

---
+++ resources/views/livewire/profile/profile.blade.php
@@ -0,0 +1,9 @@
+<div id="sidebar" class="fixed top-0 left-0 w-64 h-full bg-gray-100 shadow-lg transform -translate-x-full
+transition-transform lg:translate-x-0 lg:relative lg:w-1/3 lg:h-full flex flex-col items-center justify-center p-6">
+    <!-- Contenido del sidebar -->
+    <img src="{{ asset('img/profile.svg') }}" alt="Profile Picture" class="w-44 h-44 rounded-full mb-4">
+    <h2 class="text-xl font-bold mb-2">Yeison Alexis Velasco</h2>
+    <p class="text-gray-600 mb-1">Backend Developer</p>
+    <p class="text-gray-600 mb-4">Colombia, Risaralda</p>
+    <button class="bg-yellow-400 text-white px-4 py-2 rounded-full font-semibold">¡Contactar!</button>
+</div>
---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Sidebar</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="flex flex-col lg:flex-row h-screen">
+
+    <!-- Header (solo para pantallas pequeñas) -->
+    <header class="bg-pink-500 text-white p-4 flex items-center justify-between lg:hidden">
+        <h1 class="text-lg font-bold">Menú</h1>
+        <!-- Botón para mostrar/ocultar el sidenav -->
+        <button id="menu-toggle" class="text-2xl focus:outline-none">
+            &#9776; <!-- Icono de hamburguesa -->
+        </button>
+    </header>
+
+    <!-- Sidebar -->
+    <livewire:profile.profile/>
+
+    <!-- Content -->
+    <div class="flex-1 p-6 bg-white ">
+        <section class="text-center">
+            <h1 class="text-3xl font-bold">Acerca de mí </h1>
+            <p class="text-gray-600 mt-4 text-left">Mi nombre es Yeison Alexis Velasco Trejos, vivo en la ciudad de Pereira del departamento de Risaralda en Colombia,
+                soy Tecnologo en Analisis y Desarrollo de Sistemas de Información del SENA, con conocimientos en
+                Desarrollo Web Backend y Frontend. Con alta experiencia en el manejo de Bases de datos relaciónales
+                Actualmete trabajo en el area de tecnólogia en la empresa colombiana Chevrolet Caminos. Estoy a su disposición para serle útil y acompañarlo en este maravilloso camino del software.</p>
+
+        </section>
+    </div>
+
+    <!-- Script para el toggle del menú -->
+    <script>
+        const menuToggle = document.getElementById('menu-toggle');
+        const sidebar = document.getElementById('sidebar');
+
+        menuToggle.addEventListener('click', () => {
+            sidebar.classList.toggle('-translate-x-full'); // Oculta o muestra el sidenav en pantallas pequeñas
+        });
+    </script>
+</body>
+</html>
