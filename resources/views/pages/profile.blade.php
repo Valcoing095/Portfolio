d8eb4cdf26ae9a438ec4f89f746534350e30f8e4
Yeison <yvelasco321@gmail.com>
2025-01-03 09:29:35 -0500

    organización fonts

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -6,7 +6,7 @@
     <title>Responsive Sidebar</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="flex flex-col lg:flex-row h-screen">
+<body class="flex flex-col lg:flex-row h-screen font-serif">
 
     <!-- Header (solo para pantallas pequeñas) -->
     <header class="bg-pink-500 text-white p-4 flex items-center justify-between lg:hidden">
@@ -22,12 +22,17 @@
 
     <!-- Content -->
     <div class="flex-1 p-6 bg-white ">
-        <section class="text-center">
-            <h1 class="text-3xl font-bold">Acerca de mí </h1>
-            <p class="text-gray-600 mt-4 text-left">Mi nombre es Yeison Alexis Velasco Trejos, vivo en la ciudad de Pereira del departamento de Risaralda en Colombia,
-                soy Tecnologo en Analisis y Desarrollo de Sistemas de Información del SENA, con conocimientos en
-                Desarrollo Web Backend y Frontend. Con alta experiencia en el manejo de Bases de datos relaciónales
-                Actualmete trabajo en el area de tecnólogia en la empresa colombiana Chevrolet Caminos. Estoy a su disposición para serle útil y acompañarlo en este maravilloso camino del software.</p>
+        <section class="text-center grid grid-cols-2 gap-4">
+            <div class="flex flex-col text-justify">
+                <h1 class="text-3xl font-bold">Acerca de mí </h1>
+                <p class="text-gray-600 mt-4 ">Mi nombre es Yeison Alexis Velasco Trejos, vivo en la ciudad de Pereira del departamento de Risaralda en Colombia,
+                    soy Tecnologo en Analisis y Desarrollo de Sistemas de Información del SENA, con conocimientos en
+                    Desarrollo Web Backend y Frontend. Con alta experiencia en el manejo de Bases de datos relaciónales
+                    Actualmete trabajo en el area de tecnólogia en la empresa colombiana Chevrolet Caminos. Estoy a su disposición para serle útil y acompañarlo en este maravilloso camino del software.</p>
+            </div>
+            <div class="flex flex-col items-center pt-[4rem]">
+                <img src="{{ asset('img/profile.svg') }}" alt="Profile Picture" class="w-44 h-44 rounded-[1rem] mb-4">
+            </div>
 
         </section>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
       "./resources/**/*.vue",
     ],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+            sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+            header: ['Lato', 'sans-serif']
+        },
+      },
     },
     plugins: [],
   }
