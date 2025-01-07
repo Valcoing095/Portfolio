0b717c70bc5a4fa9c41b9fb2dd6834bbe589cde9
Yeison <yvelasco321@gmail.com>
2025-01-07 17:20:44 -0500

    Falta organizar experiencia

--- resources/views/livewire/profile/profile.blade.php
+++ resources/views/livewire/profile/profile.blade.php
@@ -1,4 +1,4 @@
-<div id="sidebar" class="absolute z-1 top-0 left-0 w-64 h-full bg-white shadow-2xl transform -translate-x-full transition-transform lg:translate-x-0 lg:relative lg:w-1/4 lg:h-screen lg:fixed flex flex-col items-center justify-center p-6 rounded-r-2xl border border-gray-200">
+<div id="sidebar" class="absolute z-50 top-0 left-0 w-64 h-full bg-white shadow-2xl transform -translate-x-full transition-transform lg:translate-x-0 lg:relative lg:w-1/4 lg:h-screen lg:fixed flex flex-col items-center justify-center p-6 rounded-r-2xl border border-gray-200">
     <!-- Imagen de perfil (opcional) -->
     {{-- <img src="{{ asset('img/profile.svg') }}" alt="Profile Picture" class="w-32 h-32 rounded-full shadow-lg mb-6"> --}}
 
@@ -15,4 +15,4 @@
     <button wire:click="downloadCV" class="bg-yellow-400 text-white px-6 py-2 rounded-full font-semibold shadow-md hover:bg-yellow-500 transition duration-300">
       Descargar CV
     </button>
-  </div>
+</div>
